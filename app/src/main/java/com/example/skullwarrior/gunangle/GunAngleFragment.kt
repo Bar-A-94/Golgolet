aff96412d21026336e98a44d56e3111aeac7788f
BarA <balon94@gmail.com>
2022-05-03 13:17:53 +0300

    code for gun angle updated
    code for gun angle updated

--- app/src/main/java/com/example/skullwarrior/gunangle/GunAngleFragment.kt
+++ app/src/main/java/com/example/skullwarrior/gunangle/GunAngleFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.skullwarrior.R
 import com.example.skullwarrior.databinding.GunAngleFragmentBinding
 
@@ -15,18 +17,45 @@ class GunAngleFragment : Fragment() {
 
     private lateinit var binding: GunAngleFragmentBinding
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var altitude = 12000F
         binding = GunAngleFragmentBinding.inflate(layoutInflater)
         binding.setLifecycleOwner(this)
         viewModel = ViewModelProvider(this).get(GunAngleViewModel::class.java)
         binding.gunAngleViewModel = viewModel
 
+        viewModel.angle.observe(viewLifecycleOwner, Observer {
+            if (it != null){
+                binding.angle.text = it
+            }
+        })
+
+        binding.button.setOnClickListener(){
+            if (binding.shooterVelocityInput.text.toString() == "" || binding.victimVelocityInput.text.toString() == ""
+                || binding.closingVelocityInput.text.toString() == "")
+            {
+                Toast.makeText(context,"Please fill in the data",Toast.LENGTH_SHORT).show()
+            } else {
+                if (binding.altitudeInput.text.toString() != ""){
+                    altitude = binding.altitudeInput.text.toString().toFloat()
+                }
+                var shooterSpeed = binding.shooterVelocityInput.text.toString().toFloat()
+                var victimSpeed = binding.victimVelocityInput.text.toString().toFloat()
+                var closingSpeed = binding.closingVelocityInput.text.toString().toFloat()
+                viewModel.onClickCalculate(shooterSpeed, victimSpeed, altitude, closingSpeed)
+                if (binding.angle.text.toString().toFloat() > 90) {
+                    Toast.makeText(context,"תותח מעל 90 עדיין פוגע",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context,"בדקת גם את הטווח?",Toast.LENGTH_SHORT)
+                }
+            }
+        }
         return binding.root
     }
-
 
 
 }
--- app/src/main/java/com/example/skullwarrior/gunangle/GunAngleViewModel.kt
+++ app/src/main/java/com/example/skullwarrior/gunangle/GunAngleViewModel.kt
@@ -1,7 +1,41 @@
 package com.example.skullwarrior.gunangle
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import kotlin.math.PI
+import kotlin.math.acos
 
 class GunAngleViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var TASShooterSpeed: Float = 0F
+    var TASVictimSpeed: Float = 0F
+    private val _angle = MutableLiveData<String>()
+    val angle: LiveData<String>
+        get() = _angle
+    val angleVisible = Transformations.map(angle) {
+        it != null
+    }
+
+    fun onClickCalculate(
+        shooterSpeed: Float,
+        victimSpeed: Float,
+        altitude: Float,
+        closingSpeed: Float
+    ) {
+
+        if (shooterSpeed < 2) {
+            TASShooterSpeed = shooterSpeed * 630
+        } else {
+            TASShooterSpeed = shooterSpeed + altitude * 5 / 1000
+        }
+        if (victimSpeed < 2) {
+            TASVictimSpeed = victimSpeed * 630
+        } else {
+            TASVictimSpeed = victimSpeed + altitude * 5 / 1000
+        }
+        val aCosInside: Float = (closingSpeed - TASShooterSpeed) / TASVictimSpeed
+        val rad = acos(aCosInside)
+        _angle.value = (rad * 180 / PI).toString()
+    }
 }
